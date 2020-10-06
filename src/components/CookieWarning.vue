09fb3864989c0638fb093328cb648faad3f897bc
Alexander Van Maele <alexander@atlesque.com>
2020-10-06 21:34:06 +0200

    Increase Cookie banner font size

--- src/components/CookieWarning.vue
+++ src/components/CookieWarning.vue
@@ -4,7 +4,7 @@
     v-if="isVisible === true"
   >
     <div class="flex flex-wrap items-center justify-around w-full max-w-2xl">
-      <span class="text-xs font-regular">
+      <span class="text-center font-regular">
         By using this site, you agree to send anonymous tracking data to help
         with development.
         <button
