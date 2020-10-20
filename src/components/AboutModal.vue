f299252a16af164a3a73513e2a961cfbb8c75681
Alexander Van Maele <alexander@atlesque.com>
2020-10-20 23:46:03 +0200

    Improve feedback form layout

--- src/components/AboutModal.vue
+++ src/components/AboutModal.vue
@@ -1,49 +1,66 @@
 <template>
-  <div>
+  <div class="flex">
     <Modal @close="closeModal">
-      <template slot="title">About</template>
-      <template slot="body">
-        <p>📝 Among Us Detective is digital notebook for Among Us</p>
-        <p>🙈 This tool is <b>not</b> a hack</p>
-        <p>
-          ✅ It is 100% legal and safe to use
-          <router-link to="/disclaimer" class="text-xs"
-            >(Read the disclaimer here)</router-link
+      <template v-if="isFeedbackScreenOpen === true">
+        <template slot="title">Feedback</template>
+        <template slot="body">
+          <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSda7OlGq68xKkVyx3GsZZntwrGN_CZZJRidgCl5J6R1QIyB2g/viewform?embedded=true"
+            width="100%"
+            height="520"
+            frameborder="0"
+            marginheight="0"
+            marginwidth="0"
+            >Loading…</iframe
           >
-        </p>
-        <h2 class="text-base font-bold">Changelog</h2>
-        <section class="p-2 overflow-y-scroll bg-gray-100 rounded max-h-64">
-          <div
-            v-for="(changelogItem, index) in changelog"
-            :key="index"
-            class="mb-4 text-sm leading-5 text-theme-gray-dark "
-          >
-            <span class="text-xs font-bold">{{ changelogItem.date }}</span>
-            <ul
-              class="pl-4 list-disc"
-              v-if="(changelogItem.changes || []).length > 0"
+        </template>
+      </template>
+      <template v-else>
+        <template slot="title">About</template>
+        <template slot="body">
+          <p>📝 Among Us Detective is digital notebook for Among Us</p>
+          <p>🙈 This tool is <b>not</b> a hack</p>
+          <p>
+            ✅ It is 100% legal and safe to use
+            <router-link to="/disclaimer" class="text-xs"
+              >(Read the disclaimer here)</router-link
             >
-              <li
-                v-for="(changeNotes, index) in changelogItem.changes"
-                :key="index"
-                v-html="changeNotes"
-              ></li>
-            </ul>
+          </p>
+          <h2 class="text-base font-bold">Changelog</h2>
+          <section class="p-2 overflow-y-scroll bg-gray-100 rounded max-h-64">
+            <div
+              v-for="(changelogItem, index) in changelog"
+              :key="index"
+              class="mb-4 text-sm leading-5 text-theme-gray-dark "
+            >
+              <span class="text-xs font-bold">{{ changelogItem.date }}</span>
+              <ul
+                class="pl-4 list-disc"
+                v-if="(changelogItem.changes || []).length > 0"
+              >
+                <li
+                  v-for="(changeNotes, index) in changelogItem.changes"
+                  :key="index"
+                  v-html="changeNotes"
+                ></li>
+              </ul>
+            </div>
+          </section>
+          <div
+            class="flex justify-between mt-6 text-xs leading-5 text-theme-gray-dark"
+          >
+            <span class="mb-0 text-xs">
+              Made with ❤ by <a href="https://atlesque.com">Atlesque</a>
+            </span>
+            <span class="text-xs">
+              <button @click="toggleFeedbackScreen">Give feedback</button>
+            </span>
           </div>
-        </section>
-        <div
-          class="flex justify-between mt-6 text-xs leading-5 text-theme-gray-dark"
-        >
-          <span class="mb-0 text-xs">
-            Made with ❤ by <a href="https://atlesque.com">Atlesque</a>
-          </span>
-          <span class="text-xs">
-            <button @click="toggleFeedbackScreen">Give feedback</button>
-          </span>
-        </div>
+        </template>
       </template>
     </Modal>
-    <Modal
+    <!-- <Modal
+      class="flex-1"
       @close="toggleFeedbackScreen"
       v-if="isFeedbackScreenOpen === true"
       :isTransparent="true"
@@ -52,15 +69,15 @@
       <template slot="body">
         <iframe
           src="https://docs.google.com/forms/d/e/1FAIpQLSda7OlGq68xKkVyx3GsZZntwrGN_CZZJRidgCl5J6R1QIyB2g/viewform?embedded=true"
-          width="518"
-          height="518"
+          width="100%"
+          height="520"
           frameborder="0"
           marginheight="0"
           marginwidth="0"
           >Loading…</iframe
         >
       </template>
-    </Modal>
+    </Modal> -->
   </div>
 </template>
 
