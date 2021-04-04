27cca23752b0d79d2348cb2674bb7fb98eb72cdc
Alexander Van Maele <alexander@atlesque.com>
2021-04-04 15:45:11 +0200

    Close #20: Save Settings to localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import VuexPersist from "vuex-persist";
 
 // Main data stores
 import crew from "./modules/crew";
@@ -8,6 +9,15 @@ import notes from "./modules/notes";
 import settings from "./modules/settings";
 
 Vue.use(Vuex);
+
+const vuexLocalStorage = new VuexPersist({
+  key: "vuex", // The key to store the state on in the storage provider.
+  storage: window.localStorage, // or window.sessionStorage or localStorage
+  // Everything below will be stored in localStorage, which persists across page reloads
+  reducer: state => ({
+    settings: state.settings,
+  }),
+});
 
 const store = new Vuex.Store({
   state: {},
@@ -19,6 +29,7 @@ const store = new Vuex.Store({
     notes,
     settings,
   },
+  plugins: [vuexLocalStorage.plugin],
 });
 
 export default store;
