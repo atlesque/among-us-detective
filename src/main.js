a83ede551d3fe274cdcb8337da29fd9215f204b8
Alexander Van Maele <alexander@atlesque.com>
2021-04-09 21:26:34 +0200

    Disable Sentry in development

--- src/main.js
+++ src/main.js
@@ -18,38 +18,40 @@ Vue.use(VueGtag, {
 });
 
 /* 
-  Sentry.js error logging
+  Sentry.js error logging in production
 */
 import * as Sentry from "@sentry/vue";
 import { Integrations } from "@sentry/tracing";
 
-Sentry.init({
-  Vue,
-  dsn:
-    "https://37f1b1f8849a4412846b47957ee5299c@o568257.ingest.sentry.io/5713136",
-  integrations: [new Integrations.BrowserTracing()],
-  /* 
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    Vue,
+    dsn:
+      "https://37f1b1f8849a4412846b47957ee5299c@o568257.ingest.sentry.io/5713136",
+    integrations: [new Integrations.BrowserTracing()],
+    /* 
     Track release number
   */
-  release: `among-us-detective@${process.env.APP_VERSION}`,
-  environment:
-    process.env.NODE_ENV === "production" ? "production" : "development",
-  /*
+    release: `among-us-detective@${process.env.APP_VERSION}`,
+    environment:
+      process.env.NODE_ENV === "production" ? "production" : "development",
+    /*
     If false, will suppress Vue's default error logging.
     Leave true for development
   */
-  logErrors: true,
-  /*
+    logErrors: true,
+    /*
     Set tracesSampleRate to 1.0 to capture 100%
     of transactions for performance monitoring.
     Use a lower value in production (e.g. 0.2)
   */
-  tracesSampleRate: 0.2,
-  /* 
+    tracesSampleRate: 0.2,
+    /* 
     If true, also track child components' performance
   */
-  trackComponents: true,
-});
+    trackComponents: true,
+  });
+}
 
 /* 
   Enable PWA support
