2b70ce8c85b8239f1ac950e3fde15e5fd933f261
Alexander Van Maele <alexander@atlesque.com>
2021-04-09 21:19:21 +0200

    Close #8: Add Sentry.js for error logging

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const SentryWebpackPlugin = require("@sentry/webpack-plugin");
+
 module.exports = {
   pwa: {
     name: "Among Us Detective",
@@ -8,6 +10,26 @@ module.exports = {
     workboxOptions: {
       skipWaiting: true,
     },
-    assetsVersion: "1.21",
+    assetsVersion: process.env.APP_VERSION,
+  },
+  // other configuration
+  configureWebpack: {
+    plugins: [
+      // Only add Sentry.js logging in production and during build time
+      ...(process.env.NODE_ENV === "production"
+        ? [
+            new SentryWebpackPlugin({
+              // sentry-cli configuration
+              authToken: process.env.SENTRY_AUTH_TOKEN,
+              org: "atlesque-media-vof",
+              project: "among-us-detective",
+
+              // webpack specific configuration
+              include: "./dist",
+              ignore: ["node_modules", "webpack.config.js"],
+            }),
+          ]
+        : []),
+    ],
   },
 };
